ca5259043f64efa858bbcb88bfffcdc4c78ef79e
nehathomas22 <thomasneha712@gmail.com>
2026-07-03 10:27:02 +0530

    Update mars.py

--- mars.py
+++ mars.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
 #take a user import\
-name=st.text_input("enter your name")
-
 st.title("take the input")
+name=st.text_input("enter your name")
 
 if st.button("submit"):
   st.write(f"print the name:{name}")
